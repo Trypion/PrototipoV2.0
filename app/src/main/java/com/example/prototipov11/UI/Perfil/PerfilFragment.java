b3d1d3141fc7af17e344450b1bd21dfc97c66218
Trypion <israel.schmitt.j@gmail.com>
2019-11-07 21:52:30 -0300

    fragmento cad de itens

--- app/src/main/java/com/example/prototipov11/UI/Perfil/PerfilFragment.java
+++ app/src/main/java/com/example/prototipov11/UI/Perfil/PerfilFragment.java
@@ -59,6 +59,7 @@ public class PerfilFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container, new CadastroFragment()).commit();
 
             }
         });
